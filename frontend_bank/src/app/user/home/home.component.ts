0b466a7bf8a37a6ae9bb7c489afd278be84b35ab
sittinon Tongsua <sittinon.to@ku.th>
2025-02-28 16:58:07 +0700

    โอเคเกือบเสร็จละ 99%

--- frontend_bank/src/app/user/home/home.component.ts
+++ frontend_bank/src/app/user/home/home.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  // สามารถเพิ่มฟังก์ชันเพื่อนำทางไปหน้าต่าง ๆ ได้
+  
 }
